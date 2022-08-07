64034b396929995d8540a12842b2475e8d6475ab
izzy417 <israeldail2@gmail.com>
2022-08-07 00:54:54 +0000

    added sign up page and in flux

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
+import { Signup } from "./pages/signup";
 import { Login } from "./pages/login";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
@@ -26,6 +27,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Login />} path="/login" />
+                        <Route element={<Signup />} path="/signup" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,100 +1,118 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			token: null, 
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+  return {
+    store: {
+      token: null,
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+
+      syncTokenFromSessionStorage: () => {
+        const token = sessionStorage.getItem("token");
+        console.log(
+          "application just loaded, syncing the session storage token"
+        );
+        if (token && token != "" && token != undefined)
+          setStore({ token: token });
+      },
+
+	  signup: async (email,password) => {
+		const opts = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
 			},
-
-			syncTokenFromSessionStorage: () => {
-				const token = sessionStorage.getItem('token');
-				console.log("application just loaded, syncing the session storage token")
-				if(token && token != "" && token != undefined) setStore({token: token})
-			
-			},
-
-			login: async (email, password) => {
-				const opts = {
-					method: "POST",
-					headers: {
-					  "Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-					  email: email,
-					  password: password,
-					}),
-				  };
-
-				  try {
-					const resp=await fetch(
-						"https://3001-israeldail-loginauthent-0vg4h4ai9c0.ws-us59.gitpod.io/api/token",
-						opts
-					  )
-					  if (resp.status !== 200) {
-						alert("there has been some error");
-						return false;
-					  } 
-	
-					  const data =await resp.json()
-					  console.log("this came from the backend", data);
-					  sessionStorage.setItem("token", data.access_token);
-					  setStore({token: data.access_token})
-					  return true;
-					}
-
-					catch(error) {
-						console.error('There has been an error logging in')
-					}
-
-
-				  },
-			  
-				  
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			body: JSON.stringify({
+				email: email,
+				password: password,
+			})
 		}
-	};
+
+		try {
+			const resp = await fetch("https://3001-israeldail-loginauthent-5k621p74mfo.ws-us59.gitpod.io/api/signup",
+            opts)
+			if(resp.status !== 200) {
+				alert('there was an error signing up')
+				return false;
+			}
+			const data = await resp.json();
+			console.log("this came from the backend", data)
+			
+		} catch(error) {
+			console.error('there has been an error with the sign up')
+		}
+	  },
+
+      login: async (email, password) => {
+        const opts = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        };
+
+        try {
+          const resp = await fetch(
+            "https://3001-israeldail-loginauthent-5k621p74mfo.ws-us59.gitpod.io/api/token",
+            opts
+          );
+          if (resp.status !== 200) {
+            alert("there has been some error");
+            return false;
+          }
+
+          const data = await resp.json();
+          console.log("this came from the backend", data);
+          sessionStorage.setItem("token", data.access_token);
+          setStore({ token: data.access_token });
+          return true;
+        } catch (error) {
+          console.error("There has been an error logging in");
+        }
+      },
+
+      logout: () => {
+        sessionStorage.removeItem("token");
+        console.log("Logging out");
+        setStore({ token: null });
+      },
+
+      getMessage: async () => {
+        const store = getStore();
+        const opts = {
+          headers: {
+            Authorization: "Bearer " + store.token,
+          },
+        };
+        //fetch request from backend
+        try {
+          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+          const data = await resp.json();
+          setStore({ message: data.message });
+          // don't forget to return something, that is how the async resolves
+          return data;
+        } catch (error) {
+          console.log("Error loading message from backend", error);
+        }
+      },
+    },
+  };
 };
 
 export default getState;
